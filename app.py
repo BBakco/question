38623ed5fd8e1ca43938b4ecc8bca37f495768e3
Yeongseon_Kim <ttjsl0101@gmail.com>
2021-10-07 00:58:01 +0900

    영선 - 메인페이지 섹션 2 슬라이드 완성

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 
 # HTML 화면 보여주기
-@app.route('/')
+@app.route('/home')
 def home():
     return render_template('mainpage.html')
 
@@ -29,6 +29,15 @@ def contents():
 
 
 # API 역할을 하는 부분
+# mainpage - section 2
+@app.route('/home/get', methods=['GET'])
+def home_get():
+    home_question = list(db.contents.find({}, {'_id': False, 'question': 1}))
+    home_answer = list(db.contents.find({}, {'_id': False, 'answer': 1}))
+
+    return jsonify({'home_q': home_question,'home_a': home_answer})
+
+# contents
 @app.route('/contents/get', methods=['GET'])
 def contents_get():
     questions = list(db.questions_ko.find({}, {'_id': False}))
