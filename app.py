c454d18980d6032a53e5f8a8cd05b0aabe8d4541
Roadman_Lee <rdmshine@gmail.com>
2021-10-06 18:22:39 +0900

    회원가입 API 완료 , 로그인 API(수정중)

--- app.py
+++ app.py
@@ -1,10 +1,84 @@
-from flask import Flask
+from pymongo import MongoClient
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+
+
+client = MongoClient('localhost', 27017)
+db = client.question
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def index():
-	return 'Hello, Flask!'
+def home():
+    return render_template('mainpage.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/mypage')
+def mypage():
+    return render_template('mypage.html')
+
+
+@app.route('/contents')
+def contents():
+    return render_template('contents.html')
+
+
+# register api
+@app.route('/register', methods=['POST'])
+def register_info():
+    username_receive = request.form['username_give']
+    email_receive = request.form['email_give']
+    pw_receive = request.form['pw_give']
+    repeatpw_receive = request.form['repeatpw_give']
+
+    if "@" not in email_receive:
+        return jsonify({'msg': '이메일을 입력해주세요.'})
+
+    elif '.' not in email_receive:
+        return jsonify({'msg': '이메일을 완성해주세요'})
+
+    elif not (email_receive and pw_receive and repeatpw_receive):
+        return jsonify({'msg': '모두 입력해주세요'})
+
+    doc = {
+        'username': username_receive,
+        'email': email_receive,
+        'pw': pw_receive,
+        'repeatpw': repeatpw_receive
+    }
+
+    db.question.insert_one(doc)
+    return jsonify({'msg': '회원가입 완료!'})
+
+# login API
+
+
+@app.route('/#pop1', methods=['POST'])
+def login():
+    email_receive = request.form['email_give']
+    pw_receive = request.form['pw_give']
+    user_data = list(db.question.find({}, {'_id': False}))
+
+    if "@" and "." not in email_receive:
+        return jsonify({"msg": "이메일을 확인해주세요"})
+    elif not email_receive and pw_receive:
+        return jsonify({'msg': '모두 입력해주세요'})
+    else:
+        for user in user_data:
+            if email_receive == user['email'] and pw_receive == user['pw']:
+                return jsonify({'msg': '환영합니다.'})
+            elif email_receive != user['email'] or pw_receive != user['pw']:
+                return jsonify({'msg': '입력하신 정보를 확인해주세요.'})
+            # elif email_receive == user['email'] and pw_receive != user['pw']:
+            #     return jsonify({'msg': '입력하신 정보를 확인해주세요.'})
+            # elif email_receive != user['email'] and pw_receive == user['pw']:
+            #     return jsonify({'msg': '입력하신 정보를 확인해주세요.'})
+
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run('0.0.0.0', port=5000, debug="true")
